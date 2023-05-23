dc20dec89ee7bbdfd082471943cc4bd45b7dd805
vaibhav patel <workwithvaibhav91@gmail.com>
2023-05-23 12:45:01 +0530

    version 4 infinite scroll added

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 export default class App extends Component {
 
+  pageSize = 3;
   render() {
     return (
       <>
@@ -18,14 +19,14 @@ export default class App extends Component {
 
         <Routes>
 
-          <Route exact path='/home' element={<News key="home" pageSize={5} country="in" category="general"/>}></Route>
-          <Route exact path='/business' element={<News key="business" pageSize={5} country="in" category="business"/>}></Route>
-          <Route exact path='/entertainment' element={<News key="entertainment" pageSize={5} country="in" category="entertainment"/>}></Route>
-          <Route exact path='/general' element={<News key="general" pageSize={5} country="in" category="general"/>}></Route>
-          <Route exact path='/health' element={<News key="health" pageSize={5} country="in" category="health"/>}></Route>
-          <Route exact path='/science' element={<News key="science" pageSize={5} country="in" category="science"/>}></Route>
-          <Route exact path='/sports' element={<News key="sports" pageSize={5} country="in" category="sports"/>}></Route>
-          <Route exact path='/technology' element={<News key="technology" pageSize={5} country="in" category="technology"/>}></Route>
+          <Route exact path='/' element={<News key="home" pageSize={this.pageSize} country="in" category="general"/>}></Route>
+          <Route exact path='/business' element={<News key="business" pageSize={this.pageSize} country="in" category="business"/>}></Route>
+          <Route exact path='/entertainment' element={<News key="entertainment" pageSize={this.pageSize} country="in" category="entertainment"/>}></Route>
+          <Route exact path='/general' element={<News key="general" pageSize={this.pageSize} country="in" category="general"/>}></Route>
+          <Route exact path='/health' element={<News key="health" pageSize={this.pageSize} country="in" category="health"/>}></Route>
+          <Route exact path='/science' element={<News key="science" pageSize={this.pageSize} country="in" category="science"/>}></Route>
+          <Route exact path='/sports' element={<News key="sports" pageSize={this.pageSize} country="in" category="sports"/>}></Route>
+          <Route exact path='/technology' element={<News key="technology" pageSize={this.pageSize} country="in" category="technology"/>}></Route>
 
         </Routes>
         
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ export class Navbar extends Component {
 
   render() {
     return (
-      <div><nav className="navbar navbar-expand-lg bg-body-tertiary bg-dark">
+      <div><nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">News</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -15,7 +15,7 @@ export class Navbar extends Component {
 
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
 
-            <li className="nav-item"><Link className="nav-link active" aria-current="page" to="/home">Home</Link></li>
+            <li className="nav-item"><Link className="nav-link active" aria-current="page" to="/general">Home</Link></li>
             <li className="nav-item"><Link className="nav-link" to="/business" >Business</Link></li>
             <li className="nav-item"><Link className="nav-link" to="/entertainment" >Entertainment</Link></li>
             <li className="nav-item"><Link className="nav-link" to="/general" >General</Link></li>
@@ -29,7 +29,8 @@ export class Navbar extends Component {
           
         </div>
       </div>
-    </nav></div>
+    </nav>
+    </div>
     )
   }
 }
